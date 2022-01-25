20b07945121b9d2e3c7febd5d8159edfd308f951
J-Mara <jmaragakis20@stuy.edu>
2022-01-25 13:19:14 -0500

    allocating memory

--- Encoder.c
+++ Encoder.c
@@ -4,19 +4,31 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 
-int main(){
-  int opfile = open("testfile.txt", O_RDONLY, 0);
+char *getCont(char* filename, char* m){
+  int opfile = open(filename, O_RDONLY, 0);
   if(opfile == -1){
     printf("there has been an error: %s\n", strerror(errno));
-    return -1;
+    return("fail");
   }else{
     struct stat s;
-    stat("testfile.txt", &s);
+    stat(filename, &s);
     int len = s.st_size;
     char res[len];
-    char *p = &res[0];
+    char *p = res;
+    p = m;
     read(opfile, p, len);
-    printf("contents of the file:\n%s\n", res);
+    close(opfile);
+    //printf("contents of the file:\n%s\n", p);
+    return p;
   }
+}
+
+int main(){
+  struct stat s;
+  stat("testfile.txt", &s);
+  char *t;
+  t = malloc(s.st_size);
+  printf("here is the contents:\n%s\n", getCont("testfile.txt", t));
 }
